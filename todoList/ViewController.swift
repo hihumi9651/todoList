81eb7a10cb54fee7f10f72fa13b167b38f57cb3c
hihumi9651 <osanaismi1203@gmail.com>
2023-07-18 22:55:38 +0900

    2023/07/18学習分

--- todoList/ViewController.swift
+++ todoList/ViewController.swift
@@ -7,13 +7,42 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    //画面ロード時に実行
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    //行数を定義
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    //セルの中身を定義
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    //セルの高さを定義
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 72
     }
 
+    //セクションの数を定義
+    func numberOfSections(in tableView: UITableView) -> Int {
+        <#code#>
+    }
+    
+    //セルをタップした時に実行
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        <#code#>
+    }
 
 }
 
